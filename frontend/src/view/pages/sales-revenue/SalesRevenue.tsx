c959fbcf9db739f3ec8d72ed149dbb4eecea4b50
ydiyora <yatidiyora13@gmail.com>
2025-01-28 08:29:06 +0000

    name change

---
+++ frontend/src/view/pages/sales-revenue/SalesRevenue.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SalesRevenue = () => {
+  return (
+    <div className="containt-management">
+        <div className="containt-management-header">
+            Search Sales Revenue
+        </div>
+    </div>
+  )
+}
+
+export default SalesRevenue
